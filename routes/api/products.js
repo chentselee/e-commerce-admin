0435ce265ad0fa10c0ed26c159ef8a7d7eeba8e1
承澤李 <chentselee@gmail.com>
2020-05-30 04:17:46 +0800

    Added feature to add new product. Added created field for product.

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,41 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Category = require("./categories");
 
 const productSchema = new mongoose.Schema({
-  category: String,
-  name: String,
-  price: Number,
+  category: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  created: { type: Date },
   image: Buffer,
 });
 
 const Product = mongoose.model("Product", productSchema);
-
-const products = [
-  { category: "phone", name: "iPhone 6", price: 5999 },
-  { category: "phone", name: "iPhone 7", price: 7999 },
-  { category: "phone", name: "iPhone 8", price: 9999 },
-];
-
-products.forEach((product) => {
-  Product.find({ name: product.name })
-    .exec()
-    .then((data) => {
-      if (data.length > 0) {
-        return;
-      } else {
-        const newProduct = new Product({
-          category: product.category,
-          name: product.name,
-          price: product.price,
-        });
-        newProduct.save((error) => {
-          if (error) console.log(error);
-        });
-      }
-    });
-});
 
 const setHeader = (req, res, next) => {
   res.set({
@@ -50,7 +25,7 @@ router.use(setHeader);
 router.get("/", (req, res) => {
   if (req.query.hasOwnProperty("id")) {
     Product.findById(req.query.id)
-      .select("category name price")
+      .select("category name price created")
       .exec()
       .then((result) => {
         res.json(result);
@@ -58,7 +33,7 @@ router.get("/", (req, res) => {
       .catch((error) => res.status(500).json({ msg: error }));
   } else if (req.query.hasOwnProperty("category")) {
     Product.find({ category: req.query.category })
-      .select("category name price")
+      .select("category name price created")
       .sort("name")
       .exec()
       .then((result) => {
@@ -67,7 +42,7 @@ router.get("/", (req, res) => {
       .catch((error) => res.status(500).json({ msg: error }));
   } else {
     Product.find()
-      .select("_id category name price")
+      .select("_id category name price created")
       .sort("name")
       .exec()
       .then((result) => {
@@ -75,6 +50,20 @@ router.get("/", (req, res) => {
       })
       .catch((error) => res.status(500).json({ msg: error }));
   }
+});
+
+router.post("/", (req, res) => {
+  const newProduct = new Product({
+    ...req.body,
+    created: new Date(),
+  });
+  newProduct.save((error) => {
+    if (error) {
+      res.status(400).json({ msg: error });
+    } else {
+      res.end();
+    }
+  });
 });
 
 module.exports = router;
---
+++ static/app.js
@@ -0,0 +1,99 @@
+const api = "http://localhost:8000";
+
+const Product = (name = "", id = "", price = "", created = "") => {
+  const card = document.createElement("div");
+  card.classList.add("card", "mb-3", "mx-auto");
+  card.innerHTML = `
+    <div class="row no-gutters">
+      <div class="col-2">
+        <img class="card-img">
+      </div>
+      <div class="col-8">
+        <div class="card-body container">
+          <dl class="row">
+            <dt class="col-4">名稱</dt>
+            <dd class="col-8">
+              <p class="card-title">${name}</p>
+            </dd>
+            <dt class="col-4">id</dt>
+            <dd class="col-8">
+              <p class="card-text">${id}</p>
+            </dd>
+            <dt class="col-4">價格</dt>
+            <dd class="col-8">
+              <p class="card-text">${price}</p>
+            </dd>
+            <dt class="col-4">新增時間</dt>
+            <dd class="col-8">
+              <p class="card-text">${created}</p>
+            </dd>
+          </dl>
+        </div>
+      </div>
+      <div class="col-2"></div>
+    </div>
+  `;
+  return card;
+};
+
+async function listProducts() {
+  const res = await fetch(`${api}/products`);
+  const products = await res.json();
+  await products.forEach((product) => {
+    const col = document.createElement("div");
+    col.classList.add("col-12");
+    col.appendChild(
+      Product(
+        product.name,
+        product._id,
+        product.price,
+        new Date(product.created).toLocaleString("zh-TW")
+      )
+    );
+    document.querySelector(".__products").appendChild(col);
+  });
+}
+
+function clearProducts() {
+  document.querySelector(".__products").innerHTML = "";
+}
+
+async function reloadProducts() {
+  clearProducts();
+  await listProducts();
+}
+
+(async function () {
+  const res = await fetch(`${api}/categories`);
+  const categories = await res.json();
+  await categories.forEach((category) => {
+    const option = document.createElement("option");
+    option.value = category.name;
+    option.innerText = category.display_name;
+    document.querySelector(".custom-select").appendChild(option);
+  });
+})();
+
+listProducts();
+
+const newProductForm = document.querySelector("#new-product");
+newProductForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const serializedFormData = {};
+  for (const [key, value] of formData.entries()) {
+    serializedFormData[key] = value;
+  }
+  const res = await fetch(`${api}/products`, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(serializedFormData),
+  });
+  if (res.ok) {
+    alert("新增成功");
+    await reloadProducts();
+    event.target.reset();
+  } else {
+    alert(`新增失敗\n${res.body.msg}`);
+  }
+});
